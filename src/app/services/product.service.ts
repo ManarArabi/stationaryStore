9c1870db791e2f06f2ea67853a4eab2a169603be
ManarArabi <manar.araby.ma@gmail.com>
2020-02-18 15:21:11 +0200

    mocks for best seller are added

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BestSeller } from '../mocks/bestSeller';
+import { Product } from '../home/bestSeller';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  constructor() { 
+    
+  }
+
+  getBestSeller():Observable<Product[]>{
+    return of(BestSeller);
+  }
+}
