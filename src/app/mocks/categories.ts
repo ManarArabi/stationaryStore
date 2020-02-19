0090fa175859547d6aef7fbd1505ecfb1c87f8a4
ManarArabi <manar.araby.ma@gmail.com>
2020-02-19 14:28:13 +0200

    side menu is added

--- src/app/mocks/categories.ts
+++ src/app/mocks/categories.ts
@@ -2,20 +2,20 @@ import { Category } from './../category';
 
 export const Categories:Category[] = [
     {
-        "categoryId": 3,
-        "categoryName": "Writing Tools",
-        "imageUrl": "https://us.123rf.com/450wm/pgmart/pgmart1706/pgmart170600043/81069378-stock-vector-color-vector-gradient-background-made-from-monochrome-squares-of-pixels-.jpg?ver=6"
+        categoryId: 3,
+        categoryName: "Writing Tools",
+        imageUrl: "https://us.123rf.com/450wm/pgmart/pgmart1706/pgmart170600043/81069378-stock-vector-color-vector-gradient-background-made-from-monochrome-squares-of-pixels-.jpg?ver=6"
     },{
-        "categoryId": 3,
-        "categoryName": "Writing Tools",
-        "imageUrl": "https://us.123rf.com/450wm/pgmart/pgmart1706/pgmart170600043/81069378-stock-vector-color-vector-gradient-background-made-from-monochrome-squares-of-pixels-.jpg?ver=6"
+        categoryId: 3,
+        categoryName: "Writing Tools",
+        imageUrl: "https://us.123rf.com/450wm/pgmart/pgmart1706/pgmart170600043/81069378-stock-vector-color-vector-gradient-background-made-from-monochrome-squares-of-pixels-.jpg?ver=6"
     },{
-        "categoryId": 3,
-        "categoryName": "Writing Tools",
-        "imageUrl": "https://us.123rf.com/450wm/pgmart/pgmart1706/pgmart170600043/81069378-stock-vector-color-vector-gradient-background-made-from-monochrome-squares-of-pixels-.jpg?ver=6"
+        categoryId: 3,
+        categoryName: "Writing Tools",
+        imageUrl: "https://us.123rf.com/450wm/pgmart/pgmart1706/pgmart170600043/81069378-stock-vector-color-vector-gradient-background-made-from-monochrome-squares-of-pixels-.jpg?ver=6"
     },{
-        "categoryId": 3,
-        "categoryName": "Writing Tools",
-        "imageUrl": "https://us.123rf.com/450wm/pgmart/pgmart1706/pgmart170600043/81069378-stock-vector-color-vector-gradient-background-made-from-monochrome-squares-of-pixels-.jpg?ver=6"
-    }
+        categoryId: 3,
+        categoryName: "Writing Tools",
+        imageUrl: "https://us.123rf.com/450wm/pgmart/pgmart1706/pgmart170600043/81069378-stock-vector-color-vector-gradient-background-made-from-monochrome-squares-of-pixels-.jpg?ver=6"
+    },
 ]
