b9374d234a2dbdb51f6149edf8e642b093afb93a
ManarArabi <manar.araby.ma@gmail.com>
2020-02-20 14:07:55 +0200

    category products design page is done

--- src/app/mocks/category_products.ts
+++ src/app/mocks/category_products.ts
@@ -12,7 +12,7 @@ export const CategoryProducts:CategoryProduct[] = [
         productId : 3,
         productName : "Pen",
         imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ5_7MuVWiQXSVNwkXwQdQ2AaAlD02oNtbYnoGcPbY0ptjAgY0G",
-        discount: 5,
+        discount: 0,
         price: 2
     },
     {
@@ -26,7 +26,7 @@ export const CategoryProducts:CategoryProduct[] = [
         productId : 3,
         productName : "Pen",
         imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ5_7MuVWiQXSVNwkXwQdQ2AaAlD02oNtbYnoGcPbY0ptjAgY0G",
-        discount: 5,
+        discount: 0,
         price: 2
     },
     {
