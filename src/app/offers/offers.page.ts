4281002388a7a972773d17589195dec7e86886e4
ManarArabi <manar.araby.ma@gmail.com>
2020-02-20 15:33:09 +0200

    offers page is ready

--- src/app/offers/offers.page.ts
+++ src/app/offers/offers.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductService } from './../services/product/product.service'; 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
   styleUrls: ['./offers.page.scss'],
 })
 export class OffersPage implements OnInit {
-
-  constructor() { }
+  Offers;
+  constructor(private ps: ProductService) { }
 
   ngOnInit() {
+    this.getOffers()
   }
 
+  getOffers():void{
+    this.ps.getBestSeller().subscribe(Products => this.Offers = Products);  }
 }
