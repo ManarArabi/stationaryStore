dc870ae9e5e1499d508a5d6f7eec866671c3c3ed
ManarArabi <manar.araby.ma@gmail.com>
2020-02-20 09:36:42 +0200

    side menu code refactoring, add icons to menu icons and make side menu close after clicking one of item items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -10,45 +11,43 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent implements OnInit {
-  public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
+      title: 'Categories',
+      url: '/categories',
+      icon: 'albums'
     },
     {
-      title: 'Favorites',
-      url: '/folder/Favorites',
+      title: 'Offers',
+      url: '',
+      icon: 'star'
+    },
+    {
+      title: 'Packages',
+      url: '',
+      icon: 'logo-buffer'
+    },
+    {
+      title: 'My Orders',
+      url: '',
       icon: 'heart'
     },
     {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
+      title: 'My Cart',
+      url: '',
+      icon: 'cart'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menu: MenuController
   ) {
     this.initializeApp();
   }
@@ -61,9 +60,9 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
-    }
+  }
+  
+  menuClose() {
+    this.menu.close()
   }
 }
