ce5640327833c6a4fee6f10dd1b934558f6cfbf9
ManarArabi <manar.araby.ma@gmail.com>
2020-02-23 16:30:03 +0200

    adding best seller api from backend and refactoring code to use it

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import { BestSeller } from '../../mocks/bestSeller';
 import { Product } from '../../types/bestSeller';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  constructor() { 
-    
+  url = 'http://192.168.1.125:8081/stationery_store_api_war';
+  constructor(private http: HttpClient) {
   }
 
-  getBestSeller():Observable<Product[]>{
-    return of(BestSeller);
+  getBestSeller():Observable<any>{
+    return this.http.get(this.url+'/api/product/bestseller?limit=6');
   }
 }
