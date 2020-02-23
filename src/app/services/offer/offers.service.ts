7663ba921cac09894f0997ce358cf6ac47dd5ad9
ManarArabi <manar.araby.ma@gmail.com>
2020-02-23 16:15:57 +0200

    remove useless code

--- src/app/services/offer/offers.service.ts
+++ src/app/services/offer/offers.service.ts
@@ -7,17 +7,9 @@ import { Observable } from 'rxjs';
 })
 export class OffersService {
   url = 'http://192.168.1.125:8081/stationery_store_api_war'
-  x ;
   constructor(private http: HttpClient) { }
 
   getOffers():Observable<any>{
-    // const headers = new Headers({
-    //   'Content-Type': 'application/json; charset=utf-8',
-    //   'Access-Control-Allow-Origin': '*',
-    //   'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
-    //   'Accept':'application/json'
-    // });
     return this.http.get(this.url+'/api/offer?limit=6');
-
   }
 }
