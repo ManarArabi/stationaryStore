1dcc83085d5c092d142b1f8e58a3f1f7502a8147
ManarArabi <manar.araby.ma@gmail.com>
2020-02-25 13:42:53 +0200

    init commit

---
+++ src/app/services/search/search.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  url = 'http://192.168.1.125:8081/stationery_store_api_war'
+
+  constructor(private http: HttpClient) { }
+  
+  search(keyWord):Observable<any>{
+    return this.http.get(this.url+'/api/search?limit=3&keyWord='+keyWord);
+  }
+}
