0691473d9eef0ae88fdae681caa560716bfbee51
ManarArabi <manar.araby.ma@gmail.com>
2020-02-25 14:35:46 +0200

    getting search input and send it to the server then navigating to search results page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../../services/search/search.service';
+import { RequestDataService } from '../../services/request-data/request-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ss: SearchService, 
+    private rds: RequestDataService,
+    private router: Router
+    ) { }
 
   ngOnInit() {}
+
+  search(keyWord){
+    this.ss.search(keyWord).subscribe( (res) => {
+      this.getSearchResults(res)
+      this.router.navigateByUrl('/search-res')
+    })
+  }
+
+  getSearchResults(res){
+    this.rds.setRequestData(res)
+  }
 
 }
