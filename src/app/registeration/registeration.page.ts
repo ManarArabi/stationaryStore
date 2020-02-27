f07a77cca13a9e8755c82d352fb8c422f265e1a7
ManarArabi <manar.araby.ma@gmail.com>
2020-02-27 10:52:32 +0200

    form control and validations are added

--- src/app/registeration/registeration.page.ts
+++ src/app/registeration/registeration.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-registeration',
@@ -6,14 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registeration.page.scss'],
 })
 export class RegisterationPage implements OnInit {
-  FirstName: string;
-  constructor() { }
+  public RegisterForm: FormGroup;
+  validation_messages = {
+    'firstName': [
+        { type: 'required', message: 'First name is required.' }
+      ],
+      'lastName': [
+        { type: 'required', message: 'Last name is required.' }
+      ],
+      'email':[
+        { type: 'required', message: 'Email is required.' },
+        { type: 'pattern', message: 'ex: example@example.com'}
+      ],
+      'password':[
+        { type: 'required', message: 'Password is required.'},
+        { type: 'minlength', message: 'min length is 5.'},
+        { type: 'pattern', message: 'upperCase, lowercase and numbers.'}
+      ]
+    };
+  constructor(public formBuilder: FormBuilder) { 
+
+    this.RegisterForm = formBuilder.group({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ])),
+      phoneNumber: new FormControl(''),
+      streetDetails: new FormControl(''),
+      building: new FormControl(''),
+      apartment: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      specialMarque: new FormControl('')
+    });
+
+  }
 
   ngOnInit() {
   }
 
-  onSubmit(){
-    console.log(this.FirstName)
+  register(){
+    console.log(this.RegisterForm.value)
   }
 
 }
