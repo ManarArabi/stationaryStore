ce1c06e58a97055fdc6560356b8665fd2f8190e3
ManarArabi <manar.araby.ma@gmail.com>
2020-03-02 11:31:32 +0200

    login form is implemented

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public LoginForm: FormGroup;
+  validation_messages = {
+      'email':[
+        { type: 'required', message: 'Email is required.' },
+        { type: 'pattern', message: 'ex: example@example.com'}
+      ],
+      'password':[
+        { type: 'required', message: 'Password is required.'}
+      ]
+    };
+  constructor(public formBuilder: FormBuilder) {
+
+    this.LoginForm = formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required
+      ]))
+    });
+
+  }
+   
 
   ngOnInit() {
   }
