d21d64678d14dd06fc5b81557d2f384b9c17ecb3
ManarArabi <manar.araby.ma@gmail.com>
2020-03-02 14:08:38 +0200

    adding profile route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,6 +42,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   }
 ];
 
