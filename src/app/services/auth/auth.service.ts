44b2b76bd836fee8665ab2f1abb96e7b17e520c6
ManarArabi <manar.araby.ma@gmail.com>
2020-03-03 11:50:53 +0200

    register and login services

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { EnvService } from './../env/env.service';
+import { User } from '../../types/UserPayLoad';
+import { UserCredentail } from '../../types/UserCredential';
+import { UserService } from './../user/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isLoggedIn = false;
+  token:any;
+
+  constructor(
+    private http: HttpClient,
+    private storage: NativeStorage,
+    private env: EnvService,
+    private userService: UserService
+  ) { }
+
+  login(user: UserCredentail): any {
+    let loggedInUser = this.userService.findUser(user)
+    if(loggedInUser != null){
+      this.token = 'hjkdfhdkjfskdjfh';
+      this.isLoggedIn = true;
+      return loggedInUser;
+    }
+    this.env.CURRENT_USER = loggedInUser;
+    return null
+  }
+
+  register(user: User) {
+    let registeredUser = this.userService.addUser(user);
+    if(registeredUser != null){
+      this.token = 'ijkfjdkfjdfkdj';
+      this.isLoggedIn = true;
+      this.env.CURRENT_USER = user
+      return user
+    }else{
+      return null
+    }
+  }
+
+  logout() {
+    this.token = '';
+    this.isLoggedIn = false;
+  }
+  
+}
