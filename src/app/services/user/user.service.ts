3e0dfafbf9076f2fec39beb2f4e10018ab6b7ead
ManarArabi <manar.araby.ma@gmail.com>
2020-03-03 11:52:35 +0200

    using it to add and remove users as virtual db

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,55 +1,39 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { throwError } from 'rxjs';
 import { User } from '../../types/UserPayLoad';
-import { CurrentUser } from '../../types/User';
-import { retry, catchError } from 'rxjs/operators';
 import { UserCredentail } from '../../types/UserCredential';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  url = 'http://192.168.1.125:8081/stationery_store_api_war';
-  constructor(private http: HttpClient) { }
+  Users: User[] = new Array();
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
+  constructor( ) { }
+
+  addUser(User: User):any{
+    let user = this.Users.filter((user) => {
+      if (user.email == User.email){
+        return user
+      }
     })
-  }
-
-  handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
+    if (user.length != 0 ){
+      console.log(user)
+      return null
     }
-    // return an observable with a user-facing error message
-    return throwError('Something bad happened; please try again later.');
-  };
 
-  register(User: User){
-    return this.http
-      .post<CurrentUser>(this.url, JSON.stringify(User), this.httpOptions)
-      .pipe(
-        retry(2),
-        catchError(this.handleError)
-      )
+    this.Users.push(User)
+    return User
   }
 
-  login(User: UserCredentail){
-    return this.http
-      .post<CurrentUser>(this.url, JSON.stringify(User), this.httpOptions)
-      .pipe(
-        retry(2),
-        catchError(this.handleError)
-      )
+  findUser(User: UserCredentail): any{
+    let user = this.Users.filter((user) => {
+      if (user.email == User.email){
+        return user
+      }
+    })
+
+    return user
   }
+
 }
 
